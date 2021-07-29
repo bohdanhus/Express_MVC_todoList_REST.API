8b6213232b49bb4483d8436d4c25538db9ac4c91
bromanovich99 <bromanovich99@gmail.com>
2021-07-29 11:00:51 +0300

    update serverhttp and add express-server

--- todo-api/src/server.js
+++ todo-api/src/server.js
@@ -1,53 +1,46 @@
-const url = require('url')
-const http = require(`http`);
+const express = require(`express`);
+const app = express()
 
-function Plural(num, nom, gen, plu) {
-    if (num % 10 == 0) {
-        return `${num} ${plu}`;
-    } else if (num == 1) {
-        return `${num} ${nom}`;
-    } else if (num % 10 > 10 && num % 10 < 20 || num % 10 == 1) {
-        return `${num} ${plu}`;
-    } else if (num % 10 >= 2 && num % 10 <= 4) {
-        return `${num}  ${gen}`;
-    } else if (num % 10 >= 5 && num % 10 <= 10) {
-        return `${num} ${plu}`;
-    }
-    return;
-};
-console.log(Plural(22, 'плюс', 'плюса', 'плюсов'))
+app.use(express.json)
 
 
-// const myObject = url.parse("localhost:3000/plural?number=2&forms=person,people,people", true).query; // { number : '2', forms: 'preson,people,people}
+function logRequest({ method, url }) {
+    console.log(`[${new Date().toISOString()}] ${method} ${url}`)
+}
+const tasks = [{ name: 'Game tasks' }, { name: 'Create task' }]
 
-const server = http.createServer((req, res) => {
-
-    if (req.url === '/headers') {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end((JSON.stringify(req.headers) + '\n'));
-
-    } else if (req.url === '/plural') {
-
-        let myObject = url.parse(req.url, true).query;
-
-        let array = Object.values(myObject);
-        let newArray = array.split(',')
-
-        const number = +newArray[0];
-        const nom = newArray[1];
-        const gen = newArray[2];
-        const plu = newArray[3];
-
-        const pl = Plural(number, nom, gen, plu)
-        res.writeHead(200, { 'Content-type': 'application/json' });
-        res.end(pl)
-    }
-
-    res.writeHead(200, { 404: 'Not found' });
-    res.end();
-
+app.get('/tasks', (req, res) => res.json(tasks))
+app.post('/tasks', (req, res) => {
+    tasks.push(task)
+    res.json(task)
 })
+app.use((req, res) => {
+    logRequest(req)
+    if (req.url === '/tasks') {
+        if (req.method == 'GET') {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(tasks));
+        } else if (req.mehthod === 'POST') {
+            const data = []
+            req.on('data', chunk => data.push(chank))
+            req.on('end', () => {
+                const task = JSON.parce(data.join(''))
+                tasks.push(task)
+                res.writeHead(201, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(tasks));
+            })
+            res.end(JSON.stringify(tasks));
+        } else {
+            res.writeHead(200, { 404: 'Not found' });
+            res.end()
+        }
+    } else {
+        res.writeHead(200, { 404: 'Not found' });
+        res.end()
+    }
+})
+
 const port = 3000
-server.listen(port, () => { //call-back сработает сразу после запуска сервера
+app.listen(port, () => { //call-back сработает сразу после запуска сервера
     console.log(`Server started at localhost:${port}`)
 });
