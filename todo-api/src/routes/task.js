e92b984a02d9474bca18b34691954e39af23f7df
bromanovich99 <73001876+bromanovich99@users.noreply.github.com>
2021-08-03 01:07:55 +0300

    Update task.js

--- todo-api/src/routes/task.js
+++ todo-api/src/routes/task.js
@@ -1,60 +1,61 @@
 const express = require('express')  
+const taskController = require('../controller/TaskController.js')
 const router = express.Router({
     mergeParams: true
 })
-const TaskController = require('../controller/TaskController.js')
-// const getIdList = require('./helperRoutes');
+const checkAnswer = (answer, res) => {
+  if (answer !== false) {
+    res.json(answer)
+  } else {
+    res.status(400)
+    res.end('Task not found')
+  }
+}
+
+const getListId = (req) => {
+  const listId = req.params.listId !== undefined ? req.params.listId : req.query.listId
+  return parseInt(listId)
+}
 
 router.get('/', function (req, res) {
+    const listId = getListId(req)
     res.status(200);
-    res.json(TaskController.getAllTasks())
+    res.json(taskController.getAllTasks())
 })
 // curl localhost:3000/tasks or http :3000/tasks true
 
-router.get('/:id', function (req, res) {
-    res.status(200);
-    res.json(TaskController.getTask(req))
+router.get('/:taskId', function (req, res) {
+    const answer = taskController.getTask(getListId(req), parseInt(req.params.taskId))
+    res.json(taskController.getTask(req))
+    checkAnswer(answer, res)
     res.end()
 })
 //curl localhost:3000/tasks/2 or http :3000/tasks/2 true
 
 router.post('/', (req, res) => {
-    res.status(200);
-    res.json(TaskController.createTask(req))
+    const answer = (taskController.createTask(getListId(req), req.body))
     res.end()
 });
 // curl localhost:3000/tasks -d '{ "name": "New task" }' -H "Content-Type: application/json"  true
 // http POST :3000/tasks task="new task"
 
-router.patch('/:id', (req, res) => {
-    res.status(200);
-    res.json(TaskController.editTask(req, res))
-    res.end()
+router.patch('/:taskId', (req, res) => {
+  const answer = taskController.updateTask(getListId(req), parseInt(req.params.taskId), req.body)
+  checkAnswer(answer, res)
 });
 // curl -X PATCH localhost:3000/tasks/1 -d '{"name": "Novoe nazvanie"}' -H "Content-Type: application/json"
 // http PATCH :3000/tasks/1 done=true true
 
-router.delete('/:id', (req, res) => {
-    const delivery = TaskController.removeTask(req, res)
-    if (delivery === true) {
-        res.status(204);
-        res.end()
-    } else {
-        res.status(404);
-        res.end()
-   }
+router.delete('/:taskId', (req, res) => {
+  const answer = taskController.removeTask(getListId(req),  parseInt(req.params.taskId))
+  checkAnswer(answer, res)
 })
 // http DELETE :3000/tasks/1 true
 
 
-router.put('/:id', (req, res) => {
-    res.status(201);
-    res.end(TaskController.changeTask(req, res))
+router.put('/:taskId', (req, res) => {
+  const answer = taskController.replaceTask(getListId(req), parseInt(req.params.taskId), req.body)
+  checkAnswer(answer, res)
 
 })
 // http PATCH :3000/tasks/1 name="" done=true
-
-
-
-module.exports = router;
-
