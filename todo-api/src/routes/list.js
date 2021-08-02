9a04a4ded169fa8e170aaab173725d728b4aaeb4
bromanovich99 <73001876+bromanovich99@users.noreply.github.com>
2021-08-03 01:07:08 +0300

    Update list.js

--- todo-api/src/routes/list.js
+++ todo-api/src/routes/list.js
@@ -1,41 +1,46 @@
 const router = require('express').Router({ mergeParams: true })
 const ListController = require('../controller/ListController.js')
 const task = require('./task')
-
+// localhost:3000/lists/:listId/tasks/:taskId
+const checkAnswer = (answer, res) => {
+  if (answer !== false) {
+    res.json(answer)
+  } else {
+    res.status(400)
+    res.end('List not found')
+  }
+}
 
 router.get('/', function (req, res) {
     res.status(200);
-    res.json(ListController.getAllLists(req))
+    res.json(listController.getAllLists(req))
     res.end()
 }) // curl localhost:3000/lists
-router.get('/:listId', function (req, res) {
-    const listId = parseInt(req.params.listId)
-    res.status(200);
-    res.json(ListController.getList(listId)
-    res.end()
-})// curl localhost:3000/lists/1
-
 router.post('/', function (req, res) {
     res.status(200);
-    res.json(ListController.createList(req))
+    res.json(listController.createList(req))
     res.end()
 }) // http POST :3000/lists
 
+
+router.get('/:listId', function (req, res) {
+    const answer = listController.getList(parseInt(req.params.listId))
+    checkAnswer(answer, res)
+})// curl localhost:3000/lists/1
+
+
 router.patch('/:listId', function (req, res) {
-    res.status(200);
-    res.json(ListController.editLists(req))
+    const answer = listController.editList(req)
+    checkAnswer(answer, res)
     res.end()
 }) //http PATCH :3000/lists/2 name="new name"
 
 router.delete('/:listId', function (req, res) {
-    const delivery = ListController.removeList(req, res)
-    if (delivery === true) {
-        res.status(204);
-        res.end()
-    } else {
-        res.status(404);
-        res.end()
-   }
-}) //http DELETE :3000/lists/2
-
+    const answer = listController.deleteList(req)
+    checkAnswer(answer, res)
+}) 
+//http DELETE :3000/lists/2
+router.use('/:listId/tasks', task)
 module.exports = router
+
+
