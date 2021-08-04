47b944a25576fe19ec690beb670586a606e529d8
bromanovich99 <bromanovich99@gmail.com>
2021-08-04 09:18:31 +0300

    rest api /list/:listId/task

--- todo-api/src/routes/list.js
+++ todo-api/src/routes/list.js
@@ -18,6 +18,18 @@ router.get('/', function (req, res) { // curl localhost:3000/lists
   }
 }) 
 
+router.get('/:listId', function (req, res) {//  localhost:3000/tasks?listId=1 
+  const showList = listController.getList(parseInt(req.params.listId))
+  if (showList) {
+    res.status(201)
+    res.json(showList)
+    res.end()
+  } else {
+    res.status(404)
+    res.end()
+  }
+})
+
 router.post('/', function (req, res) {// http POST :3000/lists  name="new list123"
   posted = listController.createList(req.body)  
   if (posted) {
@@ -29,18 +41,6 @@ router.post('/', function (req, res) {// http POST :3000/lists  name="new list12
     res.end()
   }
 }) 
-
-router.get('/:listId', function (req, res) {//localhost:3000/tasks?listId=1 
-  const showList = listController.getList(parseInt(req.params.listId))
-  if (showList) {
-    res.status(201)
-    res.json(showList)
-    res.end()
-  } else {
-    res.status(404)
-    res.end()
-  }
-})
 
 router.patch('/:listId', function (req, res) { //http PATCH :3000/lists/2 name="new name"
   const edit = listController.editList(req)
